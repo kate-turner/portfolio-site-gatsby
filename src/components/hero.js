ae2a9b1aec94d6baff16011cfe4af53cbb07a41d
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2020-09-11 22:53:36 -0600

    some things never change

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,9 +3,10 @@ import HeroImg from '../../public/static/images/hero.jpg';
 
 function Hero () {
     return (
-      <div style={{backgroundImage: `url(${HeroImg})`,  backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '300px'}}>
-          
+      <div style={{backgroundImage: `url(${HeroImg})`,  backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '800px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <div style={{textAlign: 'center'}}>
         <h1>Do da thang</h1>
+        </div>
       </div>
     )
   }
